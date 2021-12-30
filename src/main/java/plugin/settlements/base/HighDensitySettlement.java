d1995697a91d8c087d0622a7cf43d3c4758efc0a
BuildTools <unconfigured@null.spigotmc.org>
2021-12-30 21:51:16 +0100

    getters and setters

--- src/main/java/plugin/settlements/base/HighDensitySettlement.java
+++ src/main/java/plugin/settlements/base/HighDensitySettlement.java
@@ -6,9 +6,25 @@ import plugin.bank.CityBank;
 public abstract class HighDensitySettlement extends MediumDensitySettlement {
 
 	public CityBank cityBank;
-	public ArrayList<Settlement> twinTows;
+	public ArrayList<Settlement> twinTowns;
 
 	public HighDensitySettlement() {
 		super();
+	}
+
+	public CityBank getCityBank() {
+		return this.cityBank;
+	}
+
+	public void setCityBank(CityBank cityBank) {
+		this.cityBank = cityBank;
+	}
+
+	public ArrayList<Settlement> getTwinTowns() {
+		return this.twinTowns;
+	}
+
+	public void setTwinTowns(ArrayList<Settlement> twinTowns) {
+		this.twinTowns = twinTowns;
 	}
 }
